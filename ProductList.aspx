ab02fa8703776329da93f8d128adc859c24ec1fd
Zornitsa Dineva <zornitsa_angelova@yahoo.com>
2020-10-02 12:11:46 +0300

    add GetProduct()

--- ProductList.aspx
+++ ProductList.aspx
@@ -42,7 +42,7 @@
                                         </span>
                                     </a>
                                     <br />
-                                    <span>
+                                    <span class="ProductName">
                                         <b>Price: </b><%#:String.Format("{0:c}", Item.UnitPrice)%>
                                     </span>
                                     <br />
--- ProductList.aspx.cs
+++ ProductList.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using E_shop_books.Models;
+using System.Web.ModelBinding;
 
 namespace E_shop_books
 {
@@ -12,6 +14,17 @@ namespace E_shop_books
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
+        {
+            var _db = new E_shop_books.Models.ProductContext();
+            IQueryable<Product> query = _db.Products;
+            if (categoryId.HasValue && categoryId > 0)
+            {
+                query = query.Where(p => p.CategoryID == categoryId);
+            }
+            return query;
         }
     }
 }
